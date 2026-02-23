031c69799f475a6b9c646e634272a0466d86d1fc
Somnath Kar <somnathkar201901@gmail.com>
2026-02-23 21:40:05 +0530

    Updated the schema

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -2,12 +2,14 @@ const typeDefs = `#graphql
     type Author {
         id: ID!
         name: String!
+        books:[Book]
     }
 
     type Book {
         id: ID!
         title: String!
         publishedYear: Int
+        author:Author
     }
 
     type Query {
